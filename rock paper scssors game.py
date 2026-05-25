8215e3290d80d11df85a835b357131259c0edcc3
maxverstappenm820-del <maxverstappenm820@gmail.com>
2026-05-25 16:01:35 +0300

    Update rock paper scssors game.py

--- rock paper scssors game.py
+++ rock paper scssors game.py
@@ -7,10 +7,12 @@ print("Welcome to the rock, paper, scissors game!")
 
 player_score = 0
 computer_score = 0
+is_running = True
 
-while True:
+while is_running:
     player_choice = input("Enter your choice (rock, paper, scissors) or 'quit' to exit: ").lower()
     if player_choice == "quit":
+        is_running = False
         print("Thanks for playing!")
         break
     
@@ -28,9 +30,11 @@ while True:
     elif (player_choice == "rock" and computer_choice == "scissors") or (player_choice == "paper" and computer_choice == "rock") or (player_choice == "scissors" and computer_choice == "paper"):
         print("You win!")
         player_score += 1
-    else:
+    elif (player_choice == "rock" and computer_choice == "paper") or (player_choice == "paper" and computer_choice == "scissors") or (player_choice == "scissors" and computer_choice == "rock"):
         print("Computer wins!")
         computer_score += 1
+    else:
+        print("The game is a tie!")
     print(f"Score: You {player_score} - Computer {computer_score}")
 if player_score > computer_score:
     print("Congratulations! You won the game!")
